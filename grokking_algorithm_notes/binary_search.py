ca0839ad7e6936215398eecadf38fb7fb80bb768
Richard <20587893+richardtvu@users.noreply.github.com>
2021-01-19 06:19:54 -0500

    Finish copying over binary search implementation.

--- grokking_algorithm_notes/binary_search.py
+++ grokking_algorithm_notes/binary_search.py
@@ -22,6 +22,36 @@ class BinarySearch():
         
         # Item doesn't exist
         return None
+    
+    def search_recursive(self, list, low, high, item): 
+        # Check base case
+        if high >= low: 
+
+            mid = (high + low) // 2
+            guess = list[mid]
+
+            # If element is present at the middle itself
+            if guess == item: 
+                return mid
+
+            # If element is smaller than mid, then it can only 
+            # be present in left subarray
+            elif guess > item: 
+                return self.search_recursive(list, low, mid-1, item)
+            
+            # Else the element can only be present in right subarray
+            else: 
+                return self.search_recursive(list, mid+1, high, item)
+            
+if __name__ == "__main__":
+    # We must initialize the class to use the methods of this class 
+    bs = BinarySearch()
+    my_list = [1, 3, 5, 7, 9]
+
+    print(bs.search_iterative(my_list, 3)) # => 1
+
+    # 'None' means nil in Python. We use this to indicate that the item wasn't found. 
+    print(bs.search_iterative(my_list, 1)) # => None
 
     
 
