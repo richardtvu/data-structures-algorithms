2c3222320396a3218e76118b941748f7d56ed696
Richard <20587893+richardtvu@users.noreply.github.com>
2021-02-09 07:33:42 -0500

    Add pseudo-code explation for Wengrow's insertion sort algorithm

--- com-sense-dsa/insertion_sort.py
+++ com-sense-dsa/insertion_sort.py
@@ -1,18 +1,35 @@
 # Insertion Sort from p. 85 of Wengrow (2020). 
 
+"""
+Pseudocode: 
+# For each index, starting at 1:
+    # Save the value of the i-th index in a temporary variable
+    # The position for the first value to compare against the temp variable is i-1
+    # While the position still represents a valid index: 
+        # If the element in that position is greater than the temp value:
+            # Shift that element one index to the right
+            # Shift the position index one to the left 
+            # Move the position pointer to the left by one. 
+        # Else the element is less than the temp value
+            # Prepare to insert the temp value back into the array
+    # # Insert the temp value into the empty spot where it belongs
+# Return the sorted array
+""" 
+
+
 def insertion_sort(arr): 
     for i in range(1, len(arr)):
-        temp = arr[i]
-        position = i - 1 # The position, index to compare. 
-        
-        while position >= 0:         # While there are indices to check
-            if arr[position] > temp: # If the number in that index is greater than the temp variable
-                arr[position + 1] = arr[position] # Move that number to the right by one. 
-                position = position - 1 # Move the position pointer to the left by one. 
-            else: # The numbers remaining are already sorted. 
+        temp = arr[i]    
+        position = i - 1 
+
+        while position >= 0:
+            if arr[position] > temp:
+                arr[position + 1] = arr[position] 
+                position = position - 1 
+            else: 
                 break 
-        
-        arr[position + 1] = temp # Insert the temp variable we inititally took out
+
+        arr[position + 1] = temp 
     
     return arr
 
